788f8be79a9186ceda2da9e9806829236a60e2f2
Howard Chen <s880367@gmail.com>
2024-03-19 20:02:40 +0800

    answer to make the code complete

--- timsort.c
+++ timsort.c
@@ -116,13 +116,13 @@ static struct list_head *merge(void *priv,
                                struct list_head *b)
 {
     struct list_head *head;
-    struct list_head **tail = AAAA;
+    struct list_head **tail = &(head->next);
 
     for (;;) {
         /* if equal, take 'a' -- important for sort stability */
         if (cmp(priv, a, b) <= 0) {
             *tail = a;
-            tail = BBBB;
+            tail = &(a->next);
             a = a->next;
             if (!a) {
                 *tail = b;
@@ -130,7 +130,7 @@ static struct list_head *merge(void *priv,
             }
         } else {
             *tail = b;
-            tail = CCCC;
+            tail = &(b->next);
             b = b->next;
             if (!b) {
                 *tail = a;
@@ -153,8 +153,8 @@ static void build_prev_link(struct list_head *head,
     } while (list);
 
     /* The final links to make a circular doubly-linked list */
-    DDDD = head;
-    EEEE = tail;
+    tail->next = head;
+    head->prev = tail;
 }
 
 static void merge_final(void *priv,
@@ -309,7 +309,7 @@ void timsort(void *priv, struct list_head *head, list_cmp_func_t cmp)
     struct list_head *stk0 = tp, *stk1 = stk0->prev;
     while (stk1 && stk1->prev)
         stk0 = stk0->prev, stk1 = stk1->prev;
-    if (stk_size <= FFFF) {
+    if (stk_size <= 1) {
         build_prev_link(head, head, stk0);
         return;
     }
